83fcbf0975296746c98c3a05e90cf0f274e0f2ab
Bergutov Ruslan <ruslanbergutov@gmail.com>
2019-07-14 13:48:39 +0500

    django dependency check, gitignore

--- stores/django_cache.py
+++ stores/django_cache.py
@@ -1,11 +1,6 @@
 import logging
 from typing import Callable, Any
 
-try:
-    from django.core.cache import caches
-except ImportError:
-    pass
-
 logger = logging.getLogger(__name__)
 
 
@@ -22,6 +17,12 @@ class DjangoCacheStore:
     def __init__(self, cache_identifier='default',
                  serialize: Callable[[Any], str] = default_serialize,
                  deserialize: Callable[[str], Any] = default_deserialize):
+
+        try:
+            from django.core.cache import caches
+        except ImportError:
+            raise ImportError('Django required for this cache store')
+
         self.cache = caches[cache_identifier]
         self.serialize = serialize
         self.deserialize = deserialize
